88572cf91b25331699bff58a0833a217c918e700
Bartek Kryza <bkryza@gmail.com>
2023-07-05 00:41:53 +0200

    Updated docs

--- search/variables_6.js
+++ search/variables_6.js
@@ -1,14 +1,15 @@
 var searchData=
 [
-  ['generate_5flinks_0',['generate_links',['../structclanguml_1_1config_1_1inheritable__diagram__options.html#a7101d5d336c43758a272c6c0ddc71eed',1,'clanguml::config::inheritable_diagram_options']]],
-  ['generate_5fmetadata_1',['generate_metadata',['../structclanguml_1_1config_1_1inheritable__diagram__options.html#a7d89b95b57780013bf403fbdf48a8d73',1,'clanguml::config::inheritable_diagram_options']]],
-  ['generate_5fmethod_5farguments_2',['generate_method_arguments',['../structclanguml_1_1config_1_1inheritable__diagram__options.html#a13ee334c6a86a30f8b169f633ed222d5',1,'clanguml::config::inheritable_diagram_options']]],
-  ['generate_5fpackages_3',['generate_packages',['../structclanguml_1_1config_1_1inheritable__diagram__options.html#ae7ed282a85f0789f97dba4e99abda87a',1,'clanguml::config::inheritable_diagram_options']]],
-  ['generate_5freturn_5ftypes_4',['generate_return_types',['../structclanguml_1_1config_1_1inheritable__diagram__options.html#a89221b422852b721cf14b7493e3251b6',1,'clanguml::config::inheritable_diagram_options']]],
-  ['generate_5fsystem_5fheaders_5',['generate_system_headers',['../structclanguml_1_1config_1_1inheritable__diagram__options.html#aeeeb1e5095c9b10a2855803bce071705',1,'clanguml::config::inheritable_diagram_options']]],
-  ['generated_5fparticipants_5f_6',['generated_participants_',['../classclanguml_1_1sequence__diagram_1_1generators_1_1json_1_1generator.html#a0364fac96450a3ce91c25cc5e7738127',1,'clanguml::sequence_diagram::generators::json::generator::generated_participants_'],['../classclanguml_1_1sequence__diagram_1_1generators_1_1plantuml_1_1generator.html#a5f4c6a4bb2d786a7927a92ac52000e57',1,'clanguml::sequence_diagram::generators::plantuml::generator::generated_participants_']]],
-  ['generators_7',['generators',['../classclanguml_1_1cli_1_1cli__handler.html#a970b2b175accf99fe956d1639cc47e7a',1,'clanguml::cli::cli_handler']]],
-  ['git_8',['git',['../structclanguml_1_1config_1_1inheritable__diagram__options.html#ad1547712128cb611d4ac1850881ea9c8',1,'clanguml::config::inheritable_diagram_options']]],
-  ['glob_9',['glob',['../structclanguml_1_1config_1_1inheritable__diagram__options.html#ac54db8cfd5ef06c9963f2158f0494cf0',1,'clanguml::config::inheritable_diagram_options']]],
-  ['group_5fmethods_10',['group_methods',['../structclanguml_1_1config_1_1inheritable__diagram__options.html#a8b55a3dd4dc09fde08dac1162263ea0c',1,'clanguml::config::inheritable_diagram_options']]]
+  ['generate_5fcondition_5fstatements_0',['generate_condition_statements',['../structclanguml_1_1config_1_1inheritable__diagram__options.html#a2f99acf6a307527245ec3dc0520f8046',1,'clanguml::config::inheritable_diagram_options']]],
+  ['generate_5flinks_1',['generate_links',['../structclanguml_1_1config_1_1inheritable__diagram__options.html#a7101d5d336c43758a272c6c0ddc71eed',1,'clanguml::config::inheritable_diagram_options']]],
+  ['generate_5fmetadata_2',['generate_metadata',['../structclanguml_1_1config_1_1inheritable__diagram__options.html#a7d89b95b57780013bf403fbdf48a8d73',1,'clanguml::config::inheritable_diagram_options']]],
+  ['generate_5fmethod_5farguments_3',['generate_method_arguments',['../structclanguml_1_1config_1_1inheritable__diagram__options.html#a13ee334c6a86a30f8b169f633ed222d5',1,'clanguml::config::inheritable_diagram_options']]],
+  ['generate_5fpackages_4',['generate_packages',['../structclanguml_1_1config_1_1inheritable__diagram__options.html#ae7ed282a85f0789f97dba4e99abda87a',1,'clanguml::config::inheritable_diagram_options']]],
+  ['generate_5freturn_5ftypes_5',['generate_return_types',['../structclanguml_1_1config_1_1inheritable__diagram__options.html#a89221b422852b721cf14b7493e3251b6',1,'clanguml::config::inheritable_diagram_options']]],
+  ['generate_5fsystem_5fheaders_6',['generate_system_headers',['../structclanguml_1_1config_1_1inheritable__diagram__options.html#aeeeb1e5095c9b10a2855803bce071705',1,'clanguml::config::inheritable_diagram_options']]],
+  ['generated_5fparticipants_5f_7',['generated_participants_',['../classclanguml_1_1sequence__diagram_1_1generators_1_1json_1_1generator.html#a0364fac96450a3ce91c25cc5e7738127',1,'clanguml::sequence_diagram::generators::json::generator::generated_participants_'],['../classclanguml_1_1sequence__diagram_1_1generators_1_1plantuml_1_1generator.html#a5f4c6a4bb2d786a7927a92ac52000e57',1,'clanguml::sequence_diagram::generators::plantuml::generator::generated_participants_']]],
+  ['generators_8',['generators',['../classclanguml_1_1cli_1_1cli__handler.html#a970b2b175accf99fe956d1639cc47e7a',1,'clanguml::cli::cli_handler']]],
+  ['git_9',['git',['../structclanguml_1_1config_1_1inheritable__diagram__options.html#ad1547712128cb611d4ac1850881ea9c8',1,'clanguml::config::inheritable_diagram_options']]],
+  ['glob_10',['glob',['../structclanguml_1_1config_1_1inheritable__diagram__options.html#ac54db8cfd5ef06c9963f2158f0494cf0',1,'clanguml::config::inheritable_diagram_options']]],
+  ['group_5fmethods_11',['group_methods',['../structclanguml_1_1config_1_1inheritable__diagram__options.html#a8b55a3dd4dc09fde08dac1162263ea0c',1,'clanguml::config::inheritable_diagram_options']]]
 ];
